f1c678c2eb39fc0e5f63522f97d3e30f4e90b27a
rbenprieto <rbenprieto@outlook.com>
2023-01-08 22:54:38 -0500

    requirements and gitignore

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #THIRDS
+    'jazzmin',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
